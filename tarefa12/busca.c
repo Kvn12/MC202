8710dfa01e2d29ece6d6a72cf6067e328b758402
Kevin Caio <k247218@dac.unicamp.br>
2021-10-19 17:27:23 -0300

    2

--- tarefa12/busca.c
+++ tarefa12/busca.c
@@ -13,12 +13,13 @@ typedef No* p_no;
 
 p_no recebeProdutos(int n, p_no produtos);
 p_no inserirArvore(p_no raiz, char nome[21], int codigo, double valor);
-int  buscaOfertas(p_no raiz, double oferta, int codigo, int *qtd);
 p_no arvoreImpressao(p_no raiz, char nome[21], int codigo, double valor);
+int  buscaOfertas(p_no raiz, double oferta, int codigo, int *qtd);
 void imprime(p_no raiz, int codigo);
 void destroiArvore(p_no raiz);
 void imprimeArvore(p_no raiz);
 
+//Recebe os produtos e chama a funcao para inseri-los na arvore.
 p_no recebeProdutos(int n, p_no raiz){ 
     int i, codigo;
     double valor;
@@ -30,72 +31,7 @@ p_no recebeProdutos(int n, p_no raiz){
     return raiz;
 }
 
-// p_no inserirArvore(p_no raiz, char nome[21], int codigo, double valor){  
-//     p_no novo;
-//     if(raiz == NULL){
-//         novo = malloc(sizeof(No));
-//         novo->esq = novo->dir = NULL;
-//         novo->codigo = codigo;
-//         novo->valor = valor;
-//         strcpy(novo->nome, nome);
-//         return novo;
-//     }
-//     if(valor < raiz->valor){
-//         raiz->esq = inserirArvore(raiz->esq, nome, codigo, valor);
-//     }
-//     else{
-//         raiz->dir = inserirArvore(raiz->dir, nome, codigo, valor);
-//     }
-//     return raiz;
-// }
-
-// p_no buscaOfertas(p_no raiz, p_no impressao, float oferta, int codigo){
-//     while(raiz != NULL && raiz->valor < (1.1*oferta)){
-//         if(raiz->valor <= (1.1*oferta)){
-//             if(raiz->codigo == codigo){
-//             impressao = arvoreImpressao(impressao, raiz->nome, raiz->codigo, raiz->valor);
-//             }
-//             raiz = raiz->esq;
-//         }
-//         else{
-//             raiz = raiz->dir;
-//         }
-//     }
-//     return impressao;
-// }
-
-// p_no buscaOfertas(p_no raiz, p_no impressao, float oferta, int codigo){ //recursiva
-//     if(raiz == NULL){
-//         return impressao;
-//     }
-//     printf("Percorrendo: %s %d %.2f\n",raiz->nome, raiz->codigo, raiz->valor);
-//     if(raiz->valor <= (1.1*oferta)){
-//         if(raiz->codigo == codigo){
-//         impressao = arvoreImpressao(impressao, raiz->nome, raiz->codigo, raiz->valor);
-//         }
-//         buscaOfertas(raiz->esq, impressao, oferta, codigo);
-//     }
-//     // printf("AntesIF: %s %d %.2f\n",raiz->nome, raiz->codigo, raiz->valor);
-//     if(raiz->dir != NULL && raiz->dir->valor <= (1.1*oferta)){
-//         printf("SIIIIIIM\n");
-//         buscaOfertas(raiz->dir, impressao, oferta, codigo);
-//     }        
-//     return impressao;
-// }
-
-int buscaOfertas(p_no raiz, double oferta, int codigo, int *qtd){ //recursiva
-    if(raiz == NULL){
-        return *qtd;
-    }
-    buscaOfertas(raiz->esq, oferta, codigo, qtd);
-    if(raiz->valor <= (1.1*oferta) && raiz->codigo == codigo){
-        *qtd += 1;
-        printf("%s %d %.2f\n",raiz->nome, raiz->codigo, raiz->valor);
-    }       
-    buscaOfertas(raiz->dir, oferta, codigo, qtd); 
-    return *qtd;
-}
-
+//Percorre a arvore recursivamente e insere o novo produto, de acordo com a ordem alfabetica do nome das lojas.
 p_no inserirArvore(p_no raiz, char nome[21], int codigo, double valor){  
     p_no novo;
     if(raiz == NULL){
@@ -106,7 +42,7 @@ p_no inserirArvore(p_no raiz, char nome[21], int codigo, double valor){
         strcpy(novo->nome, nome);
         return novo;
     }
-    if(/*valor < raiz->valor*/strcmp(nome, raiz->nome) < 0){
+    if(strcmp(nome, raiz->nome) < 0){
         raiz->esq = inserirArvore(raiz->esq, nome, codigo, valor);
     }
     else{
@@ -115,12 +51,18 @@ p_no inserirArvore(p_no raiz, char nome[21], int codigo, double valor){
     return raiz;
 }
 
-void imprimeArvore(p_no raiz){
-    if(raiz != NULL){
-        imprimeArvore(raiz->esq);
-        printf("%s %d %.2f\n",raiz->nome, raiz->codigo, raiz->valor);
-        imprimeArvore(raiz->dir);
+//Percorre a arvore e imprime as ofertas desejadas.
+int buscaOfertas(p_no raiz, double oferta, int codigo, int *qtd){ 
+    if(raiz == NULL){
+        return *qtd;
     }
+    buscaOfertas(raiz->esq, oferta, codigo, qtd);
+    if(raiz->valor <= (1.1*oferta) && raiz->codigo == codigo){
+        *qtd += 1;
+        printf("%s %d %.2f\n",raiz->nome, raiz->codigo, raiz->valor);
+    }       
+    buscaOfertas(raiz->dir, oferta, codigo, qtd); 
+    return *qtd;
 }
 
 //Libera a memoria alocada para a arvore.
